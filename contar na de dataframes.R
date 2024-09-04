26b860f89b79d409354ab1a195b4d48fdff282a4
Natanael <natanaelguedessn@gmail.com>
2024-09-03 22:08:44 -0300

    add

---
+++ contar na de dataframes.R
@@ -0,0 +1,12 @@
+# Suponha que df seja o seu DataFrame
+missing_counts <- sapply(BOMBEIRO_BAYEUX, function(x) sum(is.na(x)))
+print(missing_counts)
+
+
+options(max.print=999999)
+
+#
+
+version #4.4.1
+getwd()
+
