675dd78bca716a011a64d403ef04e1fac5f58674
natanaelgsneto <natanaelguedessn@gmail.com>
2024-12-17 23:27:10 -0300

    add

--- Gráficos Unificados/com porcentagem/Povo Tabajara - Categoria Diversidade de expressoes culturais .py
+++ Gráficos Unificados/com porcentagem/Povo Tabajara - Categoria Diversidade de expressoes culturais .py
@@ -131,6 +131,6 @@ ax.fill(angles, values, 'b', alpha=0.1)
 
 # Add legend
 #plt.legend(loc='right', bbox_to_anchor=(-0.10, 0.1))
-plt.title("Povo Tabajara - Categoria Saberes Locais\n")
+plt.title("Povo Tabajara - Categoria Diversidade de expressões culturais\n")
 # Show the graph
 plt.show()
